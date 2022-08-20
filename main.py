76a557a499aa37dec45cd019ab6db6d8e38bb581
indicgenous <sid07102002@gmail.com>
2022-08-20 19:26:46 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+
+from flask import Flask , request , url_for , render_template 
+
+
+app=Flask(__name__,template_folder='templates')
+
+
+
+@app.route('/')
+def first():
+    return render_template('first.html')
+
+@app.route('/show')
+def show():
+    return render_template('search.html') 
+
+@app.route('/doc', methods=['POST','GET'])
+def doc():
+    loc=request.form['inp_loc']
+    return render_template('doc.html',loc=loc)
+
+@app.route('/des')
+def des():
+    return render_template('trigger.html')
+
+@app.route('/test')
+def med():
+    return render_template("med.html")
+
+if __name__=='__main__':
+    app.run(debug=True)
