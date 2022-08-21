4e3c434202dda5457a0b670e9704eed54af542f2
indicgenous <sid07102002@gmail.com>
2022-08-21 16:33:27 +0530

    deployment

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask , request , url_for , render_template 
 
-app=Flask(__name__,template_folder='templates')
+app=Flask(__name__,
+template_folder='Template')
 
 
 @app.route('/')
